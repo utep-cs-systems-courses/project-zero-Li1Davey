e03c5c8ea60f3d5342d9b208a5d0014db6300ce8
Li1Davey <dasanchez13@miners.utep.edu>
2021-09-10 16:40:41 -0600

    Created a function to make an arrrow and choose which fonts to have for draw chars.

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c Arrow = a) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -26,9 +26,31 @@ int main()
       break;
     case 'c':
       puts("You selected chars:");
-      for (char c = 'A'; c < 'D'; c++)
-	print_char_11x16(c);
+      int d;
+      puts("Select which font you want (5x7 = a, 8x12 = b, 11x16 = c)");
+      while ((d = getchar()) == '\n'); /* ignore newlines */
+      if (d == EOF)		     /* terminate on end-of-file */
+	goto done;
+      else if (d == 'a'){
+	for (char c = 'A'; c < 'D'; c++)
+	  print_char_5x7(c);
+      }
+      else if (d == 'b'){
+	for (char c = 'A'; c < 'D'; c++)
+	  print_char_8x12(c);
+      }
+      else if (d == 'c'){
+	for (char c = 'A'; c < 'D'; c++)
+	  print_char_11x16(c);
+      }
+      else{
+	printf("Unrecognized option '%c', please try again!\n", d);
+      }
       break;
+    case 'a':
+      puts("You selected arrow:");
+      print_arrow(5, 7);
+      break;  
     case 'q':
       puts("Bye!");
       goto done; 		/* terminate */
